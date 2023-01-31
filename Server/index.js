c0ead24cb69489a8bcc6334341b9491165ec0830
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-31 15:56:06 +0530

    Working in All Routes

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const config = require("./config/config");
 const connect = require("./db/db");
+const routes = require("./route/allRoutes");
 const app = express();
-
+app.use(routes);
 connect()
   .then((res) => {
-    app.listen(config.PORT)
+    app.listen(config.PORT);
     console.log(`Server is Listing on Port ${config.PORT}`);
   })
   .catch((err) => {
     console.log(err);
-  });
+});
--- Server/route/allRoutes.js
+++ Server/route/allRoutes.js
@@ -1,17 +1,56 @@
-const User=require('../model/userModel');
-const express=require('express');
-const authorization = require('../middleware/authorization');
-const route=express.Router();
+const User = require("../model/userModel");
+const bscryptjs = require("bcryptjs");
+const express = require("express");
+const authorization = require("../middleware/authorization");
+const route = express.Router();
+function generateToken(user) {
+  if (user.password) {
+    delete user.password;
+  }
+  return jwt.sign(user, config.JWT_SECRET_KEY);
+}
 
-route.post('/add-user',(req,res)=>{
+route.post("/add-user", async (req, res) => {
+  const { name, phoneNumber, password } = req.body;
+  try {
+    const exitisUser = await User.findOne({ phoneNumber });
+    if (exitisUser) {
+      return " Users Alrady exists with the given email";
+    }
+    password = bscryptjs.hashSync(password);
+    let user = await User.create({
+      name,
+      password,
+      phoneNumber,
+    });
+    user = user.toJSON();
+    delete user.password;
+    let res = generateToken(user);
+    res.send({ res: res });
+  } catch (error) {
+    res.send({ Error: "Someting went Wrong" });
+  }
+});
+route.post("/login-user", async (req, res) => {
+  try {
+    const exist = await User.findOne({
+      phoneNumber,
+    });
+    if (!exist) {
+      return " User Not Found with this email";
+    }
+    const match = bscryptjs.compareSync(password, exist.password);
 
-})
-route.post('/login-user',authorization,(req,res)=>{
-
-})
-route.post('/add-order',authorization,(req,res)=>{
-
-})
-route.get('/get-order',authorization,(req,res)=>{
-
-})
+    if (match) {
+      const token = generateToken(exist.toJSON());
+      res.send({ token });
+    } else {
+      res.send({ Error: "password is incorrect" });
+    }
+  } catch (error) {
+    res.send({ Error: "Someting went Wrong" });
+  }
+});
+route.post("/add-order", authorization, (req, res) => {});
+route.get("/get-order", authorization, (req, res) => {});
+module.exports = route;
