6b45bfb2b91a2cfabbd0979262608adf73afcfd6
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-31 15:24:36 +0530

    working on All routes

--- Server/route/allRoutes.js
+++ Server/route/allRoutes.js
@@ -3,7 +3,7 @@ const express=require('express');
 const authorization = require('../middleware/authorization');
 const route=express.Router();
 
-route.post('/add-user',authorization,(req,res)=>{
+route.post('/add-user',(req,res)=>{
 
 })
 route.post('/login-user',authorization,(req,res)=>{
