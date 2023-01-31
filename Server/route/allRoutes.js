1c5e89db9082e64b88ec983eb71b5eb6433fdc72
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-31 15:23:44 +0530

    Applying Middleware

--- Server/route/allRoutes.js
+++ Server/route/allRoutes.js
@@ -1,16 +1,17 @@
 const User=require('../model/userModel');
 const express=require('express');
+const authorization = require('../middleware/authorization');
 const route=express.Router();
 
-route.post('/add-user',(req,res)=>{
+route.post('/add-user',authorization,(req,res)=>{
 
 })
-route.post('/login-user',(req,res)=>{
+route.post('/login-user',authorization,(req,res)=>{
 
 })
-route.post('/add-order',(req,res)=>{
+route.post('/add-order',authorization,(req,res)=>{
 
 })
-route.get('/get-order',(req,res)=>{
+route.get('/get-order',authorization,(req,res)=>{
 
 })
